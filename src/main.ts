8dc2c16a7661988ed2b15de2d256bfc01dd06a8c
faluoy <faluoy@faluoy>
2023-02-16 18:08:46 +0800

    添加状态管理

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import {createApp} from 'vue'
+//导入pinia
+import {createPinia} from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
 
 import '@/styles/tailwind.css'
-import '@/styles/index.css'
-
+import '@/styles/index.less'
+//创建pinia实例
+const pinia = createPinia()
 // 5. 创建并挂载根实例
 const app = createApp(App)
 app.use(router)
+//挂载pinia
+app.use(pinia)
 app.mount('#app')
