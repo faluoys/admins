48e2d854ce658cbd719dbbb59c7b09fb42352368
faluoy <faluoy@faluoy>
2023-02-20 20:58:25 +0800

    完成轮播管理

---
+++ src/api/slide.ts
@@ -0,0 +1,80 @@
+import request from "@/utils/request";
+
+/**
+ * @description: 用户列表
+ * @param params
+ * */
+// 轮播列表
+export function slides(params: any) {
+    return request(
+        {
+            url: "/api/admin/slides",
+            method: "GET",
+            params,
+        }
+    );
+}
+
+// 轮播详情
+export function getSlidesInfo(slideid: any) {
+    return request(
+        {
+            url: `/api/admin/slides/${slideid}`,
+            method: "GET",
+        }
+    );
+}
+
+// 添加轮播
+export function addSlide(data: any) {
+    return request(
+        {
+            url: "/api/admin/slides",
+            method: "POST",
+            data,
+        }
+    );
+}
+
+// 修改轮播
+export function changeSlide(slideid: any, data: any) {
+    return request(
+        {
+            url: `/api/admin/slides/${slideid}`,
+            method: "PUT",
+            data
+        }
+    );
+}
+
+// 轮播删除
+export function delSlide(slideid: any) {
+    return request(
+        {
+            url: `/api/admin/slides/${slideid}`,
+            method: "DELETE",
+        }
+    );
+}
+
+// 轮播排序
+/*export  function updateSlide(slideid) {
+	return request(
+		{
+			url: `/api/admin/slides/${slideid}/seq`,
+			method: "PUT",
+		}
+	);
+}*/
+
+// 轮播禁用和启用
+export function getSlideLock(slideid: any) {
+    return request(
+        {
+            url: `/api/admin/slides/${slideid}/status`,
+            method: "PATCH",
+        }
+    );
+}
+
+
